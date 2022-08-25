269cb1170d20315293f2a1b06c34cbaff3cca453
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-08-25 11:24:58 +0300

    main

---
+++ Oxyjen/app/Http/Controllers/SatrtupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Satrtup;
+use Illuminate\Http\Request;
+
+class SatrtupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Satrtup  $satrtup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Satrtup $satrtup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Satrtup  $satrtup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Satrtup $satrtup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Satrtup  $satrtup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Satrtup $satrtup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Satrtup  $satrtup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Satrtup $satrtup)
+    {
+        //
+    }
+}
